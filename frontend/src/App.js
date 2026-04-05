a72476b223ffdd18c6a8d395af01c559068f1a17
Yuchen0260 <yuchen.lu@connect.qut.edu.au>
2026-04-05 17:56:04 +1000

    Add: addBook

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Profile from './pages/Profile';
-import Books from './pages/Books';
+// import Profile from './pages/Profile';
+// import Books from './pages/Books';
+
+import Dashboard from "./pages/Dashboard";
+import BooksList from "./pages/BooksList";
+import AddBook from "./pages/AddBook";
+
+
 
 function App() {
   return (
@@ -12,8 +18,11 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/Books" element={<Books />} />
+        {/* <Route path="/profile" element={<Profile />} /> */}
+        <Route path="/Books" element={<BooksList />} />
+
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/books/add" element={<AddBook />} />
       </Routes>
     </Router>
   );
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,12 +12,16 @@ const Navbar = () => {
 
   return (
     <nav className="bg-blue-600 text-white p-4 flex justify-between items-center">
-      <Link to="/" className="text-2xl font-bold">Your apps name(Library)</Link>
+      <Link to="/dashboard" className="text-2xl font-bold">
+        Your apps name(Library)
+      </Link>
+
       <div>
         {user ? (
           <>
-            <Link to="/tasks" className="mr-4">CRUD</Link>
-            <Link to="/profile" className="mr-4">Profile</Link>
+            <Link to="/dashboard" className="mr-4">Dashboard</Link>
+            <Link to="/books" className="mr-4">Books</Link>
+            <Link to="/books/add" className="mr-4">Add Book</Link>
             <button
               onClick={handleLogout}
               className="bg-red-500 px-4 py-2 rounded hover:bg-red-700"
