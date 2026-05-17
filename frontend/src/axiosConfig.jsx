e58ba577674a54cfa13ea7d5075155bfc826097a
Yuchen0260 <yuchen.lu@connect.qut.edu.au>
2026-05-17 16:56:01 +1000

    Fix axiosConfig.jsx

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://BookManager-1596291518.ap-southeast-2.elb.amazonaws.com/api', // live ALB
+  baseURL: 'http://BookManager-1596291518.ap-southeast-2.elb.amazonaws.com', // live ALB
   headers: { 'Content-Type': 'application/json' },
 });
 
